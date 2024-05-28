2250e1b41f87ca98b9c88f599b6a35088e041c68
azabarnahaju <kanikalexandra@gmail.com>
2024-05-28 15:06:54 +0200

    Migrate function views to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post
+from django.views.generic import ListView, DetailView
+
 
 # Create your views here.
 
 
-def index(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]     # since this is all converted into SQL, negative slicing does not work here!
-    return render(request, 'blog/index.html', {
-        "posts": latest_posts
-    })
+class IndexView(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    model = Post
+    ordering = ["-date"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset[:3]
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, 'blog/all-posts.html', {
-        "all_posts": all_posts
-    })
+class PostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    context_object_name = 'posts'
+    ordering = ["-date"]
+    model = Post
 
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all(),
-    })
+class PostDetailView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
